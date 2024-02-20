b5f0a1f9f300ae3963ca4927391be11922172978
jsmolos <molosj@yahoo.com>
2024-02-20 22:15:55 +0800

    1st commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const createGallery = el => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        >
+      </a>
+    </li>`;
+    })
+    .join('');
+};
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', photosMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
